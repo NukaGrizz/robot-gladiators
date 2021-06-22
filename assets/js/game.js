7063e6484293b2e5aa50e2aebbf7417032a9bc41
NukaGrizz <nukagrizz@gmail.com>
2021-06-22 16:15:47 -0400

    add js fuctions to capture robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("this is an alert! JavaScript is running");
+
+
+// this creates function named "fight"
+
+function fight() {
+    window.alert("the fight has begun");
+}
+
+var playername = window.prompt("What is your robot's name?");
+
+console.log(playername);
+console.log("This is a console log good for leaving a message for yourself");
+//this will do math and log 20
+console.log(10+10);
+console.log("Our robot's name is " + playername);
+
+
+// fight();
