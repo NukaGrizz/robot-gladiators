19501c3e9c497751cb93db073db8a6f898ab1d99
NukaGrizz <nukagrizz@gmail.com>
2021-06-30 19:49:25 -0400

    fix player name null bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,14 +72,14 @@ var startGame = function() {
         if(playerInfo.health > 0) {
             //let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
-            
+            debugger;
             //pick new enemy to fight based on the index of the enemyName array
             var pickedEnemyObj = enemyInfo[i];
 
             //reset enemyHealth before starting new fight
             pickedEnemyObj.health = randomNumber(40, 60);
 
-            ///debugger;
+            
 
             //pass the pickedEnemyName var value into the fight
             fight(pickedEnemyObj);
@@ -162,8 +162,20 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name)
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
